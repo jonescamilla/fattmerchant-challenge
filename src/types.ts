6740c973f58b9a1746a00c4588aa9c46d7daceb0
jon.escamilla <jonathanescamilla1@gmail.com>
2021-01-07 00:45:32 -0800

    intedend for `connection` branch; offer type discriptions to devs and create types for value/properties used from the resulting data of api call to `/item` (not a brialliant way to implement but a starting point)

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,68 @@
+import { InputHTMLAttributes } from 'react';
+
 /**
  * item found in @property items from @type {invoice}
- * @property {string} details
- * @property {number} quantity
- * @property {string} price
- * @property {boolean} discounted
  */
 export interface item {
+  /** name of item */
+  name: string;
+  /** the details of an item */
   details: string;
+  /** the amount of items */
   quantity: number;
+  /** the price of an item */
   price: string;
+  /** boolean on whether an item is discounted */
   discounted: boolean;
 }
+
 /**
- * @property {string} memo
- * @property {item[]} items
- * @property {string} total
+ * invoice structure/typing
  */
 export interface invoice {
+  /** memo of an invoice */
   memo: string;
+  /** collection of `item`s in an invoice */
   items: item[];
+  /** the subtotal of an invoice */
   total: string;
 }
+
+/**
+ * used or potentially used params from call to FattMerchantApi `/items`
+ */
+export interface catalog {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+  next_page_url: null | string;
+  prev_page_url: null | string;
+  data: catalogData[];
+}
+
+/**
+ * used prams in `data` found on `catalog`
+ */
+export interface catalogData {
+  id: string;
+  /** item name??? */
+  item: string;
+  /** details of item */
+  details: string;
+  /** boolean indicating of item has been discounted */
+  is_discount: boolean | null;
+  /** subtotal of item price */
+  price: number | string;
+}
+
+/**
+ * params used in custom formik components found in `formikComponents`
+ */
+export type customFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  name: string;
+  placeholder?: string;
+  precision?: number;
+  index?: number;
+};
