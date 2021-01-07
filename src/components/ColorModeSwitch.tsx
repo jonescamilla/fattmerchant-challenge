01e3a324eeb1336efde0d09c93bd048a88254c08
jon.escamilla <jonathanescamilla1@gmail.com>
2021-01-07 00:39:03 -0800

    adjust Switch to be a button w/ icons to better represent dark and light mode to users

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,10 @@
-import { useColorMode, Switch } from '@chakra-ui/react';
+import {
+  useColorMode,
+  Button,
+  Tooltip,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 /**
  * `Switch` component that handles logic for toggling colorMode used throughout application
  */
@@ -6,13 +12,18 @@ export const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === 'dark';
   return (
-    <Switch
-      position="fixed"
-      top="1rem"
-      right="1rem"
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
-    />
+    <Tooltip hasArrow label="toggle color mode">
+      <Button
+        bg=""
+        position="fixed"
+        top="1rem"
+        right="1rem"
+        color="green"
+        onClick={toggleColorMode}
+      >
+        <VisuallyHidden>Color Mode Toggle</VisuallyHidden>
+        {isDark ? <SunIcon /> : <MoonIcon />}
+      </Button>
+    </Tooltip>
   );
 };
